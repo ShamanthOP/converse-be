8079ab21d9b93acfc24622db369fea04a330d8ff
Shamanth <shamanth1604@gmail.com>
2023-05-08 19:10:37 +0530

    Added Conversation and Message model to schema

--- src/graphql/resolvers/conversation.ts
+++ src/graphql/resolvers/conversation.ts
@@ -1,4 +1,6 @@
+import { GraphQLError } from "graphql";
 import { GraphQLContext } from "../../utils/types";
+import { Prisma } from "@prisma/client";
 
 const conversationResolver = {
     Mutation: {
@@ -6,10 +8,67 @@ const conversationResolver = {
             _: any,
             args: { participantIds: Array<string> },
             context: GraphQLContext
-        ) => {
-            console.log("Craete Conversation Mutation", args);
+        ): Promise<{ conversationId: string }> => {
+            console.log("Create Conversation Mutation", args);
+            const { participantIds } = args;
+            const { session, prisma } = context;
+
+            if (!session?.user) {
+                throw new GraphQLError("Not authorized");
+            }
+            const {
+                user: { id: userId },
+            } = session;
+            try {
+                const conversations = await prisma.conversation.create({
+                    data: {
+                        participants: {
+                            createMany: {
+                                data: participantIds.map((id) => ({
+                                    userId: id,
+                                    hasSeenLastMessage: id === userId,
+                                })),
+                            },
+                        },
+                    },
+                    include: conversationPopulated,
+                });
+                return {
+                    conversationId: conversations.id,
+                };
+            } catch (e: any) {
+                console.log("createConversation Mutation Error", e);
+                throw new GraphQLError("Error creating conversation");
+            }
         },
     },
 };
+
+export const participantPopulated =
+    Prisma.validator<Prisma.ConversationParticipantInclude>()({
+        user: {
+            select: {
+                id: true,
+                username: true,
+            },
+        },
+    });
+
+export const conversationPopulated =
+    Prisma.validator<Prisma.ConversationInclude>()({
+        participants: {
+            include: participantPopulated,
+        },
+        latestMessage: {
+            include: {
+                sender: {
+                    select: {
+                        id: true,
+                        username: true,
+                    },
+                },
+            },
+        },
+    });
 
 export default conversationResolver;
